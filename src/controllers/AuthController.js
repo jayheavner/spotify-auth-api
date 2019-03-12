3705c73ae72f5061437d22f997ffb806de6764c9
Jay Heavner <jay.heavner@gmail.com>
2019-03-12 17:02:08 -0400

    end of day

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,104 +1,29 @@
-import querystring from 'querystring'
-import request from 'request'
+import querystring from 'querystring';
+import request from 'request';
 
-import config from './../../config'
-import utils from './../utils'
+import config from './../../config';
+import utils from './../utils';
 
 require('dotenv').config();
-const {
-  DeviceDiscovery
-} = require('sonos')
-const {
-  Sonos
-} = require('sonos')
 
 const AuthController = {
-
-  // sonos: function (req, res) {
-  //   console.log('sonos starting');
-
-  //   DeviceDiscovery((device1) => {
-  //     console.log('found device at ' + device.host)
-
-  //     // mute every device...
-  //     device1.setMuted(true)
-  //       .then(`${device1.host} now muted`)
-  //   })
-
-  //   console.log('here');
-
-  //   // const device = new Sonos('192.168.3.71');
-
-  //   // device.getAllGroups().then(groups => {
-  //   //   //res.send(groups);
-  //   //   console.log('All groups %s', JSON.stringify(groups, null, 2))
-  //   // }).catch(err => {
-  //   //   console.warn('Error loading topology %s', err)
-  //   // })
-
-  //   // device.zoneGroupTopologyService().GetZoneGroupAttributes().then(attributes => {
-  //   //   //res.send(attributes);
-  //   //   console.log('All Zone attributes %s', JSON.stringify(attributes, null, 2))
-  //   // }).catch(console.warn)
-
-  //   // device.getSpotifyConnectInfo().then(spotify => {
-  //   //   res.send(spotify);
-  //   // });
-
-  //   // device.pause()
-  //   //   .then(() => console.log('now playing'))
-
-  //   // find one device
-  //   DeviceDiscovery().once('DeviceAvailable', (device) => {
-  //     console.log('found device at ' + device.host)
-
-  //     // get all groups
-  //     device.getAllGroups()
-  //       .then(console.log)
-  //   })
-
-  //   // device.currentTrack()
-  //   //   .then((t) =>
-  //   //     res.send(t)
-  //   //   );
-
-  //   res.send('OK');
-  //   return;
-
-  //   // event on all found...
-  //   DeviceDiscovery((device) => {
-  //     console.log('found device at ' + device.host)
-
-  //     // mute every device...
-  //     device.setMuted(true)
-  //       .then(`${device.host} now muted`)
-  //   })
-
-  //   // find one device
-  //   DeviceDiscovery().once('DeviceAvailable', (device) => {
-  //     console.log('found device at ' + device.host)
-
-  //     // get all groups
-  //     device.getAllGroups()
-  //       .then(console.log)
-  //   })
-  // },
-
-  login: function (req, res) {
+  login: function(req, res) {
     var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-
-    console.log("----------------------------------");
-    console.log("/login");
+    const state = req.query.callback;
+    console.log('');
+    console.log('----------------------------------');
+    console.log('/login');
+    console.log(`state > ${state}`);
     console.log(`${new Date()} `);
     console.log(`url >${fullUrl}`);
-    console.log("----------------------------------");
-
-    const state = utils.generateRandomString(16);
-
     console.log(`spotify clientId => ${process.env.clientId}`);
     console.log(`spotify clientSecret => ${process.env.clientSecret}`);
+    console.log('----------------------------------');
+    console.log('');
 
-    const url = config.spotifyAccountsAPI + 'authorize?' +
+    const url =
+      config.spotifyAccountsAPI +
+      'authorize?' +
       querystring.stringify({
         response_type: 'code',
         client_id: process.env.clientId,
@@ -107,31 +32,37 @@ const AuthController = {
         state: state
       });
 
-    console.log("----------------------------------");
+    console.log('');
+    console.log('----------------------------------');
     console.log(`      url > ${url}`);
-    console.log("----------------------------------");
+    console.log('----------------------------------');
+    console.log('');
 
     res.redirect(url);
     // res.status(200).json(url);
   },
 
-  callback: function (req, res) {
+  callback: function(req, res) {
     var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-    console.log("----------------------------------");
-    console.log("      IN CALLBACK");
-    console.log('');
-    console.log(`${fullUrl}`);
-    console.log("----------------------------------");
     const code = req.query.code || null;
     const state = req.query.state || null;
+    console.log('');
+    console.log('----------------------------------');
+    console.log('      IN CALLBACK');
+    console.log('');
+    console.log(`fullUrl > ${fullUrl}`);
+    console.log(`state > ${state}`);
+    console.log('----------------------------------');
+    console.log('');
 
     if (state === null) {
-      res.redirect('/#' +
-        querystring.stringify({
-          error: 'state_mismatch'
-        }));
+      res.redirect(
+        '/#' +
+          querystring.stringify({
+            error: 'state_mismatch'
+          })
+      );
     } else {
-
       const authOptions = {
         url: config.spotifyAccountsAPI + 'api/token',
         form: {
@@ -140,71 +71,76 @@ const AuthController = {
           grant_type: 'authorization_code'
         },
         headers: {
-          'Authorization': 'Basic ' + (new Buffer(process.env.clientId + ':' + process.env.clientSecret).toString('base64'))
+          Authorization:
+            'Basic ' +
+            new Buffer(
+              process.env.clientId + ':' + process.env.clientSecret
+            ).toString('base64')
         },
         json: true
       };
 
-      request.post(authOptions, function (error, response, body) {
+      request.post(authOptions, function(error, response, body) {
         if (!error && response.statusCode === 200) {
-          const {
-            access_token,
-            refresh_token,
-            expires_in
-          } = body;
+          const { access_token, refresh_token, expires_in } = body;
 
-          console.log("----------------------------------");
+          console.log('');
+          console.log('----------------------------------');
           console.log(` access_token > ${access_token}`);
-          console.log("----------------------------------");
-          console.log("----------------------------------");
-          console.log(` redirecting to > ${config.clientURL}`);
-          console.log("----------------------------------");
+          console.log('----------------------------------');
+          console.log('----------------------------------');
+          console.log(` redirecting to > ${state}`);
+          console.log('----------------------------------');
+          console.log('');
 
-          res.redirect('http://localhost:8080/spotify/callback?' +
-            querystring.stringify({
-              access_token,
-              refresh_token,
-              expires_in
-            }));
+          // res.send({
+          //   access_token: access_token,
+          //   refresh_token: refresh_token,
+          //   expires_in: expires_in
+          // });
 
-          // res.redirect('http://localhost:8081/spotify/callback?' +
-          //   querystring.stringify({
-          //     access_token: access_token,
-          //     refresh_token: refresh_token,
-          //     expires_in: expires_in,
-          //     scope: scope
-          //   }));
-
-          // res.redirect(config.clientURL + '#/login?' +
-          //   querystring.stringify({
-          //     access_token,
-          //     refresh_token,
-          //     expires_in
-          //   }));
+          res.redirect(
+            `${state}/?` +
+              querystring.stringify({
+                access_token,
+                refresh_token,
+                expires_in
+              })
+          );
         } else {
-          res.redirect('/#' +
-            querystring.stringify({
-              error: 'invalid_token'
-            }));
+          res.redirect(
+            '/#' +
+              querystring.stringify({
+                error: 'invalid_token'
+              })
+          );
         }
       });
     }
   },
 
-  refreshToken: function (req, res) {
-    console.log("----------------------------------");
-    console.log("      IN refreshToken");
-    console.log("----------------------------------");
+  refreshToken: function(req, res) {
+    console.log('');
+    console.log('----------------------------------');
+    console.log('      IN refreshToken');
+    console.log('----------------------------------');
+    console.log('');
 
     const refresh_token = req.query.refresh_token;
-    console.log("----------------------------------");
+    console.log('');
+    console.log('----------------------------------');
     console.log(` refresh_token > ${refresh_token}`);
-    console.log("----------------------------------");
+    console.log('----------------------------------');
+    console.log('');
 
     const authOptions = {
       url: config.spotifyAccountsAPI + 'api/token',
       headers: {
-        'Authorization': 'Basic ' + (new Buffer(process.env.clientId + ':' + process.env.clientSecret).toString('base64'))
+        Authorization:
+          'Basic ' +
+          new Buffer(
+            process.env.clientId + ':' + process.env.clientSecret
+          ).toString('base64')
       },
       form: {
         grant_type: 'refresh_token',
@@ -213,31 +149,26 @@ const AuthController = {
       json: true
     };
 
-    request.post(authOptions, function (error, response, body) {
+    request.post(authOptions, function(error, response, body) {
       if (!error && response.statusCode === 200) {
         const access_token = body.access_token;
         const expires_in = body.expires_in;
 
-        res.redirect('http://localhost:8080/spotify/refresh_token?' +
-          querystring.stringify({
-            access_token,
-            expires_in
-          }));
-
-        // res.send({
-        //   'access_token': access_token
-        // });
+        res.send({
+          access_token: access_token,
+          expires_in: expires_in
+        });
       }
     });
   },
-  sonosHook: function (req, res) {
+  sonosHook: function(req, res) {
     var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
     //console.log(`  url > ${fullUrl}`);
     //console.log(req.body);
     res.send(req.body);
   },
 
-  generic: function (req, res) {
+  generic: function(req, res) {
     console.log('GENERIC');
     var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
     console.log(`  url > ${fullUrl}`);
--- src/index.js
+++ src/index.js
@@ -1,21 +1,47 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import tcpPortUsed from 'tcp-port-used';
+import portscanner from 'portscanner';
 import config from '../config';
 import middleware from './middleware';
 import routes from './routes';
-import bodyParser from 'body-parser';
+import { SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION } from 'constants';
 
 const app = express();
 const port = process.env.PORT || config.port;
 
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-app.use(bodyParser.json());
+(async () => {
+  console.log('Checking port 4040');
 
-app.listen(port);
+  try {
+    const portStatus = await tcpPortUsed.check(4040, '0.0.0.0');
+    console.log(`port ${port} open: ${portStatus}`);
+    if (portStatus) {
+      console.log('use the following commands to find and kill the processes');
+      console.log('netstat -ano | findstr :4040');
+      console.log('taskkill /PID {id} /F');
+      const free = await tcpPortUsed.waitUntilFree(4040, 200, 1000 * 60);
+      console.log(`Port ${port} is now free.`);
+    }
+    resume();
+  } catch (err) {
+    console.error('Error on check:', err.message);
+  }
+})();
 
-middleware(app, express);
+const resume = () => {
+  app.use(
+    bodyParser.urlencoded({
+      extended: false
+    })
+  );
+  app.use(bodyParser.json());
 
-routes(app);
+  app.listen(port);
 
-console.log(`Listen on port: ${port}`);
+  middleware(app, express);
+
+  routes(app);
+
+  console.log(`Listen on port: ${port}`);
+};
